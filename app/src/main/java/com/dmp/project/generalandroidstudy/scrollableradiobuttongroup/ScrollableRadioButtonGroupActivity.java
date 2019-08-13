9010743f6c9e42611fc54ce9299e091c3ecf65cc
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2019-08-14 00:50:26 +0800

    custom radiobutton study

--- app/src/main/java/com/dmp/project/generalandroidstudy/scrollableradiobuttongroup/ScrollableRadioButtonGroupActivity.java
+++ app/src/main/java/com/dmp/project/generalandroidstudy/scrollableradiobuttongroup/ScrollableRadioButtonGroupActivity.java
@@ -3,6 +3,11 @@ package com.dmp.project.generalandroidstudy.scrollableradiobuttongroup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.dmp.project.generalandroidstudy.R;
 
 public class ScrollableRadioButtonGroupActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class ScrollableRadioButtonGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrollable_radio_button_group);
+
+
+        RadioGroup daily_weekly_button_view = findViewById(R.id.daily_weekly_button_view);
+
+        final RadioButton radio0 = findViewById(R.id.radio0);
+        radio0.setOnCheckedChangeListener(new CustomCheckChangedListener());
+
     }
 }
