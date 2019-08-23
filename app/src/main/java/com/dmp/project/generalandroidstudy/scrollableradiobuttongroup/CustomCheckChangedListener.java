7e55c48f6892e2ec3f254bb6a73b9b7e3f00e75f
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2019-08-23 14:07:12 +0800

    general study2

--- app/src/main/java/com/dmp/project/generalandroidstudy/scrollableradiobuttongroup/CustomCheckChangedListener.java
+++ app/src/main/java/com/dmp/project/generalandroidstudy/scrollableradiobuttongroup/CustomCheckChangedListener.java
@@ -6,15 +6,15 @@ import android.widget.RadioGroup;
 
 import com.dmp.project.generalandroidstudy.R;
 
-public class CustomCheckChangedListener implements CompoundButton.OnCheckedChangeListener {
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-        Resources resources = compoundButton.getResources();
-        if(isChecked){
-            compoundButton.setTextColor(resources.getColor(R.color.white));
-        }else{
-            compoundButton.setTextColor(resources.getColor(R.color.blue));
+    public class CustomCheckChangedListener implements CompoundButton.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+            Resources resources = compoundButton.getResources();
+            if(isChecked){
+                compoundButton.setTextColor(resources.getColor(R.color.white));
+            }else{
+                compoundButton.setTextColor(resources.getColor(R.color.blue));
 
+            }
         }
     }
-}
